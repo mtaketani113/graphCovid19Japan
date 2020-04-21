0e256d891fdb994665e829eb8ac052fe9104295f
mtaketani113 <take1131012@gmail.com>
2020-04-21 21:25:00 +0900

    コードのスタイルチェックを変更

--- barGraph.py
+++ barGraph.py
@@ -1,31 +1,34 @@
+"""日本の感染者の推移を棒グラフで表示する."""
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import readFiles as rf
-import datetime
+import datetime as dt
 
-#json読み込み
-json = rf.readJsonFile('japan.json')
+# json読み込み
+json = rf.read_json_file('japan.json')
 
-leftList = list()
-deathsList = list()
-recoveredList = list()
-activeList = list()
+left_list = list()
+deaths_list = list()
+recovered_list = list()
+active_list = list()
 
 
-#jsonをリストに変換
-for dailyData in json:
-    leftList.append(datetime.datetime.strptime(dailyData["Date"], '%Y-%m-%dT%H:%M:%SZ'))
-    deathsList.append(dailyData["Deaths"])
-    recoveredList.append(dailyData["Recovered"])
-    activeList.append(dailyData["Confirmed"] - dailyData["Recovered"] - dailyData["Deaths"])
+# jsonをリストに変換
+for daily in json:
+    left_list.append(dt.datetime.strptime(daily["Date"], '%Y-%m-%dT%H:%M:%SZ'))
+    deaths_list.append(daily["Deaths"])
+    recovered_list.append(daily["Recovered"])
+    # 感染者全体から死者と回復者を引き、闘病数を計算
+    active_number = daily["Confirmed"] - daily["Recovered"] - daily["Deaths"]
+    active_list.append(active_number)
 
-left = np.asarray(leftList)
-deaths = np.asarray(deathsList)
-recovered = np.asarray(recoveredList)
-active = np.asarray(activeList)
+left = np.asarray(left_list)
+deaths = np.asarray(deaths_list)
+recovered = np.asarray(recovered_list)
+active = np.asarray(active_list)
 
-#棒グラフでプロット
+# 棒グラフでプロット
 plt.title("total number of infected and deaths in Japan")
 plt.xlabel("Date")
 plt.ylabel("Cases")
@@ -35,9 +38,8 @@ p2 = plt.bar(left, recovered, bottom=deaths, color="green")
 p3 = plt.bar(left, active, bottom=recovered, color="blue")
 plt.legend((p1[0], p2[0], p3[0]), ("deaths", "recovered", "active"))
 
-#get current axes
+# get current axes
 ax = plt.gca()
- 
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%d-%b-%Y'))
 ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1))
 ax.xaxis.set_minor_locator(mdates.MonthLocator(interval=1))
--- readFiles.py
+++ readFiles.py
@@ -1,8 +1,19 @@
+"""Read Json file."""
 import json
 
-#jsonファイルを読み込みjsonを返却
-def readJsonFile(fileName):
-    json_open = open(fileName, 'r')
+# jsonファイルを読み込みjsonを返却
+
+
+def read_json_file(file_path):
+    """Read Json File.
+
+        Args:
+            file_path: 入力ファイルパス
+
+        Returns:
+            output- 読み込んだファイルをJson形式
+    """
+    json_open = open(file_path, 'r')
     json_load = json.load(json_open)
 
     return json_load
